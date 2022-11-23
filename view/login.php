f3bdc5138e08db4097368bf404074bad3f9fcc00
binhtrong4354t <trongsbph21504@fpt.edu.vn>
2022-11-23 15:58:01 +0700

    .
    .

--- view/login.php
+++ view/login.php
@@ -1,39 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Đăng nhập barber poly</title>
-        <link rel="stylesheet" href="../public/css/login.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-</head>
-<body>
- <div class="container">
-        <div class="login-box">
-                <h2>Đăng nhập</h2>
-                <form>
-                  <div class="user-box">
-                    <input type="text" name="" required="">
-                    <label>Số Điện Thoại</label>
-                  </div>
-                  <div class="user-box">
-                    <input type="password" name="" required="">
-                    <label>Mật Khẩu </label>
-                  </div>
-                  <a href="#">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    Submit
-                  </a>
-                </form>
-              </div>
-        </div>
 
-      <script src="../public/main.js"></script>  
-</body>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Đăng nhập barber poly</title>
+  <link rel="stylesheet" href="../public/css/login.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+</head>
+
+<body>
+
+  <body class="body-service">
+    <header class="other-service-header">
+      <div class="logo-box">
+        <img class="logo" src="../public/img/logo.png" alt="" height="120px">
+
+        <div class="menu-main">
+          <ul class="menu">
+            <li><a href="">Trang chủ</a></li>
+            <li><a href="">Phản Hồi</a></li>
+          </ul>
+          <div>
+            <button class="knot"><span><img src="../public/img/icon/30shine.38a2a504.svg" alt=""></span> Đăng
+              nhập </button>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <div class="login-box">
+        <h2>Đăng nhập</h2>
+        <form>
+          <div class="user-box">
+            <input type="text" name="" required="">
+            <label>Số Điện Thoại</label>
+          </div>
+          <div class="user-box">
+            <input type="password" name="" required="">
+            <label>Mật Khẩu </label>
+          </div>
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Submit
+          </a>
+        </form>
+      </div>
+    </div>
+   
+
+    <script src="../public/main.js"></script>
+  </body>
+
 </html>
