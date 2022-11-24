98401fedf85e33875b496bda9c0cba04a39ee913
binhtrong4354t <trongsbph21504@fpt.edu.vn>
2022-11-24 08:17:13 +0700

    đặt lịch
    chưa hoàn thành

---
+++ view/datLich.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+</head>
+</head>
+
+<body>
+
+    <body class="body-service">
+        <header class="other-service-header">
+            <div class="logo-box">
+                <img class="logo" src="../public/img/logo.png" alt="" height="120px">
+
+                <div class="menu-main">
+                    <ul class="menu">
+                        <li><a href="">Trang chủ</a></li>
+                        <li><a href="">Phản Hồi</a></li>
+                    </ul>
+                    <div>
+                        <button class="knot"><span><img src="../public/img/icon/30shine.38a2a504.svg" alt=""></span> Đăng
+                            nhập </button>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="container-layout">
+           
+                <h3 class="new-top-navigator">Đặt lịch</h3>
+                <div class="main-creen">
+                    <h3 class="font-creen">1.Salon</h3>
+                    <div class="creen-item">
+                        <span><img src="../public/img/icon/homeOutline.76facfa5.svg" alt=""></span>
+
+                        <p>Tòa nhà FPT Polytechnic, P. Trịnh Văn Bô, Xuân Phương, Nam Từ Liêm, Hà Nội, Việt Nam</p>
+                    </div>
+
+                </div>
+                <div class="block">
+                    <h3 class="font-medium"> 2.Chọn dịch vụ</h3>
+                    <div class="creen-item">
+                        <span><img src="../public/img/icon/service.3a62b101.svg" alt=""></span>
+                        <select name="dichvu" id="" class="choose-service">
+                                <optgroup label="Chọn Dịch Vụ">
+                                        <option value="" class="choose">Xin mời chọn dịch vụ</option>
+                                <option value="">Cắt Tóc mát xoa</option>
+                                <option value="">Uốn</option>
+                                <option value="">Nhuộm</option>
+                                <option value="">gội đầu</option>
+                                </optgroup>
+                        </select>
+                       
+                    </div>
+
+                </div>
+           
+            
+        </div>
+        <footer class="footer">
+            <div class="container">
+                <div class="footer-main">
+                    <div class="info">
+                        <p class="info-item">CÔNG TY CỔ PHẦN 5 THÀNH VIÊN:))</p>
+                        <p>
+                            Tòa nhà FPT Polytechnic, P. Trịnh Văn Bô, Xuân Phương, Nam Từ Liêm, Hà Nội, Việt Nam <br>
+                            Số giấy chứng nhận kinh doanh: xxx.xxxx.xxx <br>
+                            Ngày cấp: 0x/0x/xxx <br>
+                            Nơi cấp: Sở kế hoạch đầu tư TP Hà Nội</p>
+                        <a href="" class="info-item">Chính sách bảo mật thông tin
+                        </a> <br>
+                        <a href="" class="info-item">Liên hệ quảng cáo barber poly</a>
+                        <p>TRUNG TÂM ĐÀO TẠO NGHỀ TÓC Barber <br>
+                            Địa chỉ: <br>
+                            - Hà Nội: Tòa nhà FPT Polytechnic, P. Trịnh Văn Bô, Xuân Phương, Nam Từ Liêm, Hà Nội, Việt
+                            Nam
+                            <br>
+                            Liên hệ học nghề tóc: xxxx.xx.xxxx
+                        </p>
+                    </div>
+                    <div class="time">
+                        <ul class="time-detal">
+                            <li>GIỜ PHỤC VỤ </li>
+                            <li>Thứ 2 đến thứ 6: 8h00 - 21h00</li>
+                            <li>Thứ 7, chủ nhật: 7h30 - 21h00</li>
+                        </ul>
+                        <div class="phone">
+                            <img src="../public/img/icon/phone-2.aecf5e0a.svg" alt="">
+                            <span>HOTLINE: 1800.xx.xx.xx (Miễn phí)</span>
+                        </div>
+                        <div class="app-cash">
+                            <span class="app-cash__label">Chấp nhận thanh toán</span>
+                            <div class="app-cash__list">
+                                <div class="app-cash__item">
+                                    <img src="../public/img/icon/visa.689e1cc5.svg" alt="">
+                                </div>
+                                <div class="app-cash__item"><img src="../public/img/icon/mastercard.7d32e23f.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+
+</html>
