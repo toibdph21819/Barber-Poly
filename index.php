b36d9b9be4d04d1fbfa0eef98939ead3af072e62
nobrain <64298592+nobrain1412@users.noreply.github.com>
2022-11-26 13:30:10 +0700

    route

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
-require_once('connection.php');
 
-if (isset($_GET['controller'])) {
-  $controller = $_GET['controller'];
-  if (isset($_GET['action'])) {
-    $action = $_GET['action'];
-  } else {
-    $action = 'index';
-  }
-} else {
-  $controller = 'pages';
-  $action = 'home';
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '\app\views\index.php';
+        break;
+    case '' :
+        require __DIR__ . '\app\views\index.php';
+        break;
+    case '/login' :
+        require __DIR__ . '\app\views\pages\login.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '\app\views\pages\404.php';
+        break;
 }
-require_once('routes.php');
